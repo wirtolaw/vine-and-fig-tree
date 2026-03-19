52e7a7de39be04684dd656a0c12c3692339ba5dc
Peiru Wang <p.wang@tilburguniversity.edu>
2026-03-19 19:27:21 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vft-v1';
+const CACHE_NAME = 'vft-v3';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
@@ -8,19 +8,18 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((names) =>
       Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const fetched = fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const clone = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
         return response;
-      });
-      return cached || fetched;
-    })
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || new Response('Offline')))
   );
 });
