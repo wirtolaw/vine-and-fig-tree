53486506eb183cdbbe8f1a62fa345b9ed9edb02f
Peiru Wang <p.wang@tilburguniversity.edu>
2026-03-19 10:16:11 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'vft-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+      return cached || fetched;
+    })
+  );
+});
